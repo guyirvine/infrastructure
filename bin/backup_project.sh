e5e81fb2efe784453601727bd1a27e715c45c4a3
Guy Irvine <girvine@lic.co.nz>
2015-06-04 05:39:51 +1200

    Remove schema based tar

--- bin/backup_project.sh
+++ bin/backup_project.sh
@@ -8,7 +8,7 @@ find "$WORKING_DIR" -iname 'project.*.tar.bz2' -type f -mtime +6 -delete
 
 pg_dump --host=172.17.42.1 --username=projectuser project >> $WORKING_DIR/project.sql || exit 1
 
-ls $WORKING_DIR/project.*.sql | xargs tar -jcf "$WORKING_DIR/$COMPRESSED_FILE_NAME" -C "$WORKING_DIR" || exit 2
+tar -jcf "$WORKING_DIR/$COMPRESSED_FILE_NAME" -C "$WORKING_DIR" project.sql || exit 2
 rm $WORKING_DIR/project.*.sql || exit 3
 
 rm -f "$WORKING_DIR/$FILE_NAME.latest.tar.bz2" || exit 4

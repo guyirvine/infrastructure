e95975d858e0159b3bca423a8cae625d1e014d29
Guy Irvine <girvine@lic.co.nz>
2015-06-04 06:06:29 +1200

    Bugfix: write, rather than cat backup

--- bin/backup_project.sh
+++ bin/backup_project.sh
@@ -6,7 +6,7 @@ WORKING_DIR=/guyirvine.com/backup
 
 find "$WORKING_DIR" -iname 'project.*.tar.bz2' -type f -mtime +6 -delete
 
-pg_dump --host=172.17.42.1 --username=projectuser project >> $WORKING_DIR/project.sql || exit 1
+pg_dump --host=172.17.42.1 --username=projectuser project > $WORKING_DIR/project.sql || exit 1
 
 tar -jcf "$WORKING_DIR/$COMPRESSED_FILE_NAME" -C "$WORKING_DIR" project.sql || exit 2
 rm $WORKING_DIR/project.sql || exit 3

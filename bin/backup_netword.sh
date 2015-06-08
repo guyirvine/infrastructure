5451994c1b7a18a9a436c69898025b3c1f5b508a
Guy Irvine <girvine@lic.co.nz>
2015-06-09 09:22:54 +1200

    Added backup_netword

---
+++ bin/backup_netword.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE_NAME=project.sql
+COMPRESSED_FILE_NAME=project.$(date +%Y%m%d%H%M).tar.bz2
+WORKING_DIR=/guyirvine.com/backup
+
+find "$WORKING_DIR" -iname 'project.*.tar.bz2' -type f -mtime +6 -delete
+
+pg_dump --host=172.17.42.1 --username=projectuser project > $WORKING_DIR/project.sql || exit 1
+
+tar -jcf "$WORKING_DIR/$COMPRESSED_FILE_NAME" -C "$WORKING_DIR" project.sql || exit 2
+rm $WORKING_DIR/project.sql || exit 3
+
+rm -f "$WORKING_DIR/$FILE_NAME.latest.tar.bz2" || exit 4
+ln -s "$WORKING_DIR/$COMPRESSED_FILE_NAME" "$WORKING_DIR/$FILE_NAME.latest.tar.bz2" || exit 5
